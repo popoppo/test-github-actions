d5e70efe646b3d5386f9e98872a6c03e176f1e74
popoppo <popoppo39@gmail.com>
2021-01-25 01:47:55 +0000

    Version 0.11.8-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hello "0.11.7"
+(defproject hello "0.11.8-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
